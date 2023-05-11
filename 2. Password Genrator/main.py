2b94666d75b7c9aea691bb43d7ce14047659d5ad
Jayesh Vegda <130431390+JayeshVegda@users.noreply.github.com>
2023-05-11 17:00:49 +0530

    Passgen Final

--- 2. Password Genrator/main.py
+++ 2. Password Genrator/main.py
@@ -1,19 +1,22 @@
 import random
 import time
 import os
+import ctypes
 
 class PasswordGenerator:
-    
+
     def __init__(self):
         self.password = ""
         self.n = ""
         self.default_n = 4
         self.default_word = 3
+        self.default_complexes = 0
         self.default_symbol = ['-', '$', '&']
         self.symbol_list = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '[', ']', '{', '}', ';', ':', '"', "'", '<', '>', ',', '.', '/', '?', '|', '\\', '`', '~',]
         self.word_list = ['a', 'an', 'the', 'and', 'but', 'or', 'if', 'then', 'while', 'for', 'to', 'in', 'of', 'with', 'on', 'at', 'by', 'from', 'up', 'about', 'into', 'over', 'after', 'beneath', 'under', 'above', 'near', 'before', 'beside', 'between', 'behind', 'among', 'around', 'through', 'during', 'until', 'against', 'across', 'outside', 'inside', 'onto', 'into', 'toward', 'upon', 'off', 'over', 'out', 'down', 'up', 'on', 'off', 'out', 'down', 'up', 'on', 'off', 'out', 'down', 'very', 'so', 'such', 'just', 'only', 'now', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'what', 'who', 'whom', 'whose', 'which', 'that', 'this', 'these', 'those', 'my', 'your', 'his', 'her', 'its', 'our', 'their', 'mine', 'yours', 'his', 'hers', 'its', 'ours', 'theirs', 'me', 'you', 'him', 'her', 'it', 'us', 'them', 'that', 'which', 'who', 'whom', 'whose', 'anybody', 'anyone', 'anything', 'each', 'either', 'everybody', 'everyone', 'everything', 'neither', 'nobody', 'no one', 'nothing', 'one', 'somebody', 'someone', 'something', 'both', 'few', 'many', 'several', 'all', 'any', 'more', 'most', 'some', 'none', 'such', 'every', 'no', 'least', 'own' ] 
         self.custom_word_list = []
-        self.Alphabates_list = ['a', ]
+        self.number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+        self.Alphabates_list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
         
     def random_with_N_digits(self, n):
         range_start = 10**(n-1)
@@ -21,7 +24,7 @@ class PasswordGenerator:
         return random.randint(range_start, range_end)
     
 
-    def logo(self):
+    def logo(self):  
         print(""" 
                 
         ░██████╗░███████╗███╗░░██╗  ██████╗░░█████╗░░██████╗░██████╗
@@ -49,11 +52,11 @@ class PasswordGenerator:
         if gen_choice == 1:
             self.pin()
         elif gen_choice == 2:
-            self.complex()
+            self.complexes()
         elif gen_choice == 3:
             self.memoriable()
         elif gen_choice == 4:
-            self.start()
+            self.exito()
         else:
             print("Invalid Choice")
             time.sleep(1)
@@ -90,18 +93,33 @@ class PasswordGenerator:
         except:
             print("Error Occure")
 
-    def complex(self):
+    def complexes(self):
         self.my_logo()
         complex_password = ""
-        word_length = input(f"\nEnter the length of Password (Default=8 Max=32) : ")
-        ask_number = input("Do you want to use numbers (y/n): ")
-        ask_symbol = input("Do you want to use special Symbol (y/n): ")
-        ask_alpha = input("Do you want to use Alphabates (y/n): ")
+        if self.default_complexes == 0:
+            word_length = int(input(f"\nEnter the length of Password (Default=8 Max=32) : "))
+        else:
+            word_length = self.default_complexes
+            print("Your Default Length is : ", word_length)
+            
+        input("Please Enter to Genrate your Password....")
         
+        for i in range(word_length):
+            word = random.choice(self.Alphabates_list)
+            symbol = random.choice(self.symbol_list)
+            number = random.randint(1, 10)
+            complex_password += word + symbol + str(number)
+            final = complex_password[0:word_length]
         
-        
-        
-    
+        print("\nYour Genrated Password is : ", final)
+        leave = input("\nEnter to Continue or write exit to leave :")
+        if leave.strip() == "":
+            self.complexes()
+        else:
+             print("Returning to main menu")
+             time.sleep(1)  
+             self.start()
+   
     def continue_memoriable(self):
         leave = input("\nEnter to Continue or write exit to leave :")
         if leave.strip() == "":
@@ -113,18 +131,21 @@ class PasswordGenerator:
         self.my_logo()
         memoriable_password = ""
         word_length = input(f"\nEnter the minimun word list (default = {self.default_word}) : ")
-        ask_word = input("Do you want to add use custom words list (y/n): ")
-        if ask_word == "y":
-            while True:
-                word = input("Enter the word (Leave blank to exit): ")
-                if word.strip() == "":
-                    break
-                else:
-                    self.custom_word_list.append(word)
-            print(self.custom_word_list)
+        if self.custom_word_list == []:
+            ask_word = input("Do you want to add use custom words list (y/n): ")
+            if ask_word == "y":
+                while True:
+                    word = input("Enter the word (Leave blank to exit): ")
+                    if word.strip() == "":
+                        break
+                    else:
+                        self.custom_word_list.append(word)
+                print(self.custom_word_list)
+            else:
+                print("Continue.... \n")
         else:
-            print("Continue.... \n")
-        
+            ask_word = "y"
+            
         ask_symbol = input("Do you want to add special Symbol (y/n): ")
         if ask_symbol == "y":
             print("yes")
@@ -159,7 +180,7 @@ class PasswordGenerator:
             for i in range(word_length):
                 word = random.choice(word_list)
                 memoriable_password += word
-            print(memoriable_password)
+            print("Your Genrated Password is \n ", memoriable_password)
             self.continue_memoriable()
             
         elif ask_word != "y" and ask_symbol == "y":
@@ -172,7 +193,7 @@ class PasswordGenerator:
                 word = random.choice(word_list)
                 symbol = random.choice(symbol_list)
                 memoriable_password += word + symbol
-            print(memoriable_password)
+            print("Your Genrated Password is \n ", memoriable_password)
             self.continue_memoriable()
             
         elif ask_word != "y" and ask_symbol != "y":
@@ -182,7 +203,7 @@ class PasswordGenerator:
             for i in range(word_length):
                 word = random.choice(word_list)
                 memoriable_password += word
-            print(memoriable_password)
+            print("Your Genrated Password is \n ", memoriable_password)
             self.continue_memoriable()
             
         else:
@@ -191,17 +212,66 @@ class PasswordGenerator:
 
 
     def setting(self):
-        pass
+        self.my_logo()
+        print("\nSelect any one from the menu below: ")
+        print("1. Change Default Length of Password")
+        print("2. Add Custom Words List")
+        print("3. Return to Main Menu")
+        
+        choice = int(input("Enter your choice: "))
+        
+        if choice == 1:
+            selected_length = int(input("Enter the length of password : "))            
+            self.default_complexes = selected_length
+            print(f"Your Default Length is {selected_length}, {self.default_complexes}")
+            input("Plese Press Enter to Return Main Menu")
+            time.sleep(1)
+            self.setting()
+        elif choice == 2:
+            while True:
+                word = input("Enter the word (Leave blank to exit): ")
+                if word.strip() == "":
+                    break
+                else:
+                    self.custom_word_list.append(word)
+            print(self.custom_word_list)
+            input("Plese Press Enter to Return Main Menu")
+            time.sleep(1)
+            self.setting()
+        elif choice == 3:
+            self.start()
+        else:
+            print("Invalid Choice")
+            time.sleep(1)
+            self.setting()
+        
     
     def credit(self):
-        pass
-    
+        os.system("cls")
+        self.my_logo()
+        print("""\n- Created By: Jayesh Vegda \n- Github: https://github.com/Jayesh.Vegda \n- Email: Jayeshvegda@gmail.com \n- Twitter: https://twitter.com/Jayesh.Vegda \n- Instagram: https://www.instagram.com/Zayu.Vegda \n""")
+        input("\nPlease Enter to Return to Main Menu: ")
+        time.sleep(1)
+        self.start()
+            
     def exito(self):
-        pass    
+        os.system("cls")
+        self.my_logo()
+        print("\n Good Bye ! Have a Good Day ahead")
+        time.sleep(2)
+        exit()
     
     def start(self):
+        os.system('cls' if os.name == 'nt' else 'clear')  # Clear the screen
+        if os.name == "nt":  # If the system is windows
+            print("")
+            ctypes.windll.kernel32.SetConsoleTitleW(
+                f"Password Genrator - Mafe by Jayesh Vegda")  # Change the
+        else:  # Or if it is unix
+            print(f'\33]0;Password Genrator - Made by Jayesh Vegda\a',
+                  end='', flush=True)  # Update title of command prompt
         os.system("cls")
-        self.logo()
+        self.my_logo()
         time.sleep(1)
         print("\nSelect Any one from the menu below: ")
         print("1. Generate Password")
@@ -218,7 +288,7 @@ class PasswordGenerator:
         elif choice == "3":
             self.credit()
         elif choice == "4":
-            self.exit()
+            self.exito()
         else:
             print("Invalid Choice")
             time.sleep(1)
